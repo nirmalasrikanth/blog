27248fe57e9eb6854d4ed37e9b4852cfd5bf4820
Nirmala <nnirmalasri@gmail.com>
2021-11-14 10:48:06 -0600

    included domain name

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n1_0c)#3!h(nzg1%4z94u2+yz@!fv@&l&v*-rwxk)+dr(v7(mn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '147.182.251.209']
+ALLOWED_HOSTS = ['localhost', '147.182.251.209', 'www.physicsprep.xyz', 'physicsprep.xyz']
 
 SITE_ID = 1
 
